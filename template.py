a41f8fec9de4532dc889fd33a44651ae0f8deda2
Apurv Chudasama <120238040+Apurv090405@users.noreply.github.com>
2024-09-14 13:14:23 +0530

    Update template.py

--- template.py
+++ template.py
@@ -1,2 +1,16 @@
 import os
 from pathlib import Path
+
+# Lists of files
+list_of_files=[
+    "./github/workflows",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/traning_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+]
